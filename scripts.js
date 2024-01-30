26949bbf9d8a32a3998d4f5102e430c28c71160c
Patrick Dolan <dolanp1992@gmail.com>
2024-01-30 11:48:42 -0800

    Add styles, scripts, and update HTML structure

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// UI Logic
+function handleShowRulesClick() {
+  alert("Rules button clicked!");
+}
+
+function handlePlayAgainClick() {
+  alert("Play again button clicked!");
+}
+
+// Window load listener
+window.addEventListener("load", function() {
+  document.getElementById("rules-button").addEventListener("click", handleShowRulesClick);
+  document.getElementById("play-again-button").addEventListener("click", handlePlayAgainClick);
+});
