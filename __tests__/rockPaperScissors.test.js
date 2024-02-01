ec36af23169111e838779c5fd9c9654d2103b26e
Patrick Dolan <dolanp1992@gmail.com>
2024-02-01 09:10:34 -0800

    refactor RockPaperScissor instance name and add handlePlayerTurns method

--- __tests__/rockPaperScissors.test.js
+++ __tests__/rockPaperScissors.test.js
@@ -2,65 +2,78 @@ import RockPaperScissors from '../src/js/rockPaperScissors.js';
 import Player from '../src/js/player.js';
 
 describe("RockPaperScissors", () => {
-  let game;
+  let rockPaperScissors;
   let player1;
   let player2;
 
   beforeEach(() => {
     player1 = new Player("Player1");
     player2 = new Player("Player2");
-    game = new RockPaperScissors(player1, player2);
+    rockPaperScissors = new RockPaperScissors(player1, player2);
   });
 
   test("should create a RockPaperScissors object with two players, a winning combos object, and a valid choices array", () => {
-    expect(game.players.length).toEqual(2);
-    expect(game.winningCombinations).toEqual({
+    expect(rockPaperScissors.players.length).toEqual(2);
+    expect(rockPaperScissors.winningCombinations).toEqual({
       rock: "scissors",
       paper: "rock",
       scissors: "paper"
     });
-    expect(game.validChoices).toEqual(["rock", "paper", "scissors"]);
+    expect(rockPaperScissors.validChoices).toEqual(["rock", "paper", "scissors"]);
   });
 
   describe("isValidChoice", () => {
     test("should return true if the choice is valid", () => {
-      expect(game.isValidChoice("rock")).toEqual(true);
+      expect(rockPaperScissors.isValidChoice("rock")).toEqual(true);
     });
 
     test("should return false if the choice is invalid", () => {
-      expect(game.isValidChoice(null)).toEqual(false);
+      expect(rockPaperScissors.isValidChoice(null)).toEqual(false);
     });
   });
 
   describe("determineWinner", () => {
     test("should return 'Player1' if Player1's choice beats Player2's choice", () => {
-      game.players[0].choice = "rock";
-      game.players[1].choice = "scissors";
-      expect(game.determineWinner()).toEqual(player1.name);
+      rockPaperScissors.players[0].choice = "rock";
+      rockPaperScissors.players[1].choice = "scissors";
+      expect(rockPaperScissors.determineWinner()).toEqual(player1.name);
     });
 
     test("should return 'Player2' if Player2's choice beats Player1's choice", () => {
-      game.players[0].choice = "rock";
-      game.players[1].choice = "paper";
-      expect(game.determineWinner()).toEqual(player2.name);
+      rockPaperScissors.players[0].choice = "rock";
+      rockPaperScissors.players[1].choice = "paper";
+      expect(rockPaperScissors.determineWinner()).toEqual(player2.name);
     });
 
     test("should return 'tie' if Player1's choice ties Player2's choice", () => {
-      game.players[0].choice = "rock";
-      game.players[1].choice = "rock";
-      expect(game.determineWinner()).toEqual("tie");
+      rockPaperScissors.players[0].choice = "rock";
+      rockPaperScissors.players[1].choice = "rock";
+      expect(rockPaperScissors.determineWinner()).toEqual("tie");
     });
 
     test("should return 'invalid choice' if player2's choice is invalid", () => {
-      game.players[0].choice = "rock";
-      game.players[1].choice = "invalid";
-      expect(game.determineWinner()).toEqual("invalid choice");
+      rockPaperScissors.players[0].choice = "rock";
+      rockPaperScissors.players[1].choice = "invalid";
+      expect(rockPaperScissors.determineWinner()).toEqual("invalid choice");
     });
 
     test("should return 'invalid choice' if player1's choice is invalid", () => {
-      game.players[0].choice = "invalid";
-      game.players[1].choice = "rock";
-      expect(game.determineWinner()).toEqual("invalid choice");
+      rockPaperScissors.players[0].choice = "invalid";
+      rockPaperScissors.players[1].choice = "rock";
+      expect(rockPaperScissors.determineWinner()).toEqual("invalid choice");
+    });
+  });
+
+  describe("handlePlayerTurns", () => {
+    test("should switch the currentTurn property to the the second player index", () => {
+      rockPaperScissors.handlePlayerTurns();
+      expect(rockPaperScissors.currentTurn).toEqual(1);
+    });
+
+    test("should switch the currentTurn property to the first player index", () => {
+      rockPaperScissors.currentTurn = 1;
+      rockPaperScissors.handlePlayerTurns();
+      expect(rockPaperScissors.currentTurn).toEqual(0);
     });
   });
 });
